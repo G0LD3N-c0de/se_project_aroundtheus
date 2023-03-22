66d70e5e61f34800cdb27b25ef6f39e823f06e05
SpaceMan28 <nicokazemi34@gmail.com>
2023-03-22 10:17:59 -0600

    add modal

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,31 @@
 let initialCards = [
-  (objectOne = {
+  {
     name: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
-  }),
-  (objectTwo = {
+  },
+
+  {
     name: "Lake Louise",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
-  }),
-  (objectThree = {
+  },
+
+  {
     name: "Bald Mountains",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
-  }),
-  (objectFour = {
+  },
+
+  {
     name: "Latemar",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
-  }),
-  (objectFive = {
+  },
+
+  {
     name: "Vanoise National Park",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
-  }),
-  (objectSix = {
+  },
+
+  {
     name: "Lago di Braies",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg ",
-  }),
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+  },
 ];
