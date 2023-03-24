ee5644e38f045354d84a0c4d14e2594bf9bcedad
SpaceMan28 <nicokazemi34@gmail.com>
2023-03-24 14:27:53 -0600

    Add JavaScript to open/close modal

--- scripts/index.js
+++ scripts/index.js
@@ -3,29 +3,36 @@ let initialCards = [
     name: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
   },
-
   {
     name: "Lake Louise",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
   },
-
   {
     name: "Bald Mountains",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
   },
-
   {
     name: "Latemar",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
   },
-
   {
     name: "Vanoise National Park",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
   },
-
   {
     name: "Lago di Braies",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+
+let modal = document.querySelector(".modal");
+let editButton = document.querySelector(".profile__rectangle");
+let modalCloseButton = document.querySelector(".modal__close");
+
+editButton.addEventListener("click", function () {
+  modal.classList.add("modal__opened");
+});
+
+modalCloseButton.addEventListener("click", function () {
+  modal.classList.remove("modal__opened");
+});
