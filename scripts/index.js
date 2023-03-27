01e15abcf584d974ddb505ac97cec27d6223f9aa
SpaceMan28 <nicokazemi34@gmail.com>
2023-03-27 16:49:39 -0600

    feat: add JavaScript functionality to webpage

--- scripts/index.js
+++ scripts/index.js
@@ -25,20 +25,58 @@ let initialCards = [
   },
 ];
 
+// Elements
+
 let modal = document.querySelector(".modal");
 let editButton = document.querySelector(".profile__rectangle");
 let modalCloseButton = document.querySelector(".modal__close");
+let profileTitle = document.querySelector(".profile__title");
+let profileDescription = document.querySelector(".profile__description");
+let modalTitle = document.querySelector(".modal__input-title");
+let modalDescription = document.querySelector(".modal__input-description");
+let modalForm = modal.querySelector(".modal__form");
+let cardTemplate =
+  document.querySelector(".card__template").content.firstElementChild;
+let cardList = document.querySelector(".gallery__cards");
+
+// Functions
+
+function closePopup() {
+  modal.classList.remove("modal__opened");
+}
+
+function getCardElement(data) {
+  let cardElement = cardTemplate.cloneNode(true);
+  let cardTitleEl = cardElement.querySelector(".card__title");
+  let cardImageEl = cardElement.querySelector(".card__image");
+  cardImageEl.src = data.link;
+  cardImageEl.alt = data.name;
+  cardTitleEl.textContent = data.name;
+  return cardElement;
+}
+
+// Event Listeners
 
 editButton.addEventListener("click", function () {
   modal.classList.add("modal__opened");
+  modalTitle.value = profileTitle.textContent;
+  modalDescription.value = profileDescription.textContent;
 });
+
 modalCloseButton.addEventListener("click", function () {
-  modal.classList.remove("modal__opened");
+  closePopup();
 });
 
-let modalTitle = document.querySelector(".modal__input-title");
-let modalDescription = document.querySelector(".modal__input-description");
+modalForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  profileTitle.textContent = modalTitle.value;
+  profileDescription.textContent = modalDescription.value;
+  closePopup();
+});
 
-modalTitle.value = document.querySelector(".profile__title").textContent;
-modalDescription.value =
-  document.querySelector(".profile__subtitle").textContent;
+// Handler
+
+initialCards.forEach((data) => {
+  cardElement = getCardElement(data);
+  cardList.prepend(cardElement);
+});
