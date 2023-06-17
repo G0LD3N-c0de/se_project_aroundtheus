78bbf3409c6666b3a8916ab973d899a44570b5fa
SpaceMan28 <nicokazemi34@gmail.com>
2023-06-16 18:20:09 -0600

    create Api class

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Section from "../components/Section.js";
 import PopupWithForm from "../components/PopupWithForm.js";
 import PopupWithImage from "../components/PopupWithImage.js";
 import UserInfo from "../components/UserInfo.js";
+import Api from "../components/Api.js";
 import {
   initialCards,
   editProfileButton,
